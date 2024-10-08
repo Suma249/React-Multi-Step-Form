bbd4fc52e4c75898540ce93277cef758549e1870
Suma249 <3584_suma@katalystindia.org>
2024-10-08 12:21:38 +0530

    corrected the error in confirm component with values props

--- src/Components/Confirn.js
+++ src/Components/Confirn.js
@@ -1,78 +1,80 @@
 import React, { Component } from 'react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import AppBar from 'material-ui/AppBar'
-import {List, ListItem} from 'material-ui/List'
+import { List, ListItem } from 'material-ui/List'
 import RaisedButton from 'material-ui/RaisedButton'
 
 export class Confirn extends Component {
-    continue = e =>{
+    continue = e => {
         e.preventDefault();
         //process form
 
         this.props.nextStep();
     }
-    back = e =>{
+    back = e => {
         e.preventDefault();
         this.props.prevStep();
     }
 
-    
-  render() {
-    //const {values, handleChange}=this.props;
-    const {
-        values:{firstName,lastName,email,occupation,city,bio}
-    }=this.props
-    return (
-        <MuiThemeProvider>
-           <React.Fragment>
-            <AppBar title='Confirm User Data'/>
-            <List>
-                <ListItem
+
+    render() {
+        //const {values, handleChange}=this.props;
+        const {
+            values: { firstName, lastName, email, occupation, city, bio }
+        } = this.props
+        console.log("props: " + this.props.values)
+        // console.log("values: " + values);
+        return (
+            <MuiThemeProvider>
+                <React.Fragment>
+                    <AppBar title='Confirm User Data' />
+                    <List>
+                        {/* <ListItem
                 primaryText='first name'
                 secondaryText={firstName}
-                />
-                <ListItem
-                primaryText='last name'
-                secondaryText={lastName}
-                />
-                <ListItem
-                primaryText='email'
-                secondaryText={email}
-                />
-                <ListItem
-                primaryText='occupation'
-                secondaryText={occupation}
-                />
-                <ListItem
-                primaryText='city'
-                secondaryText={city}
-                />
-                <ListItem
-                primaryText='bio'
-                secondaryText={bio}
-                />
-            </List>
-            <RaisedButton
-            label='Confirm and Continue'
-            primary={true}
-            style={styles.button}
-            onClick={this.continue}
-            />
-             <RaisedButton
-            label='Back'
-            primary={false}
-            style={styles.button}
-            onClick={this.back}
-            />
-           </React.Fragment>
-      </MuiThemeProvider>
-    )
-  }
+                /> */}
+                        <ListItem
+                            primaryText='last name'
+                            secondaryText={lastName}
+                        />
+                        <ListItem
+                            primaryText='email'
+                            secondaryText={email}
+                        />
+                        <ListItem
+                            primaryText='occupation'
+                            secondaryText={occupation}
+                        />
+                        <ListItem
+                            primaryText='city'
+                            secondaryText={city}
+                        />
+                        <ListItem
+                            primaryText='bio'
+                            secondaryText={bio}
+                        />
+                    </List>
+                    <RaisedButton
+                        label='Confirm and Continue'
+                        primary={true}
+                        style={styles.button}
+                        onClick={this.continue}
+                    />
+                    <RaisedButton
+                        label='Back'
+                        primary={false}
+                        style={styles.button}
+                        onClick={this.back}
+                    />
+                </React.Fragment>
+            </MuiThemeProvider>
+        )
+    }
 }
 
-const styles={
+const styles = {
     button: {
-        margin:15
+        margin: 15
     }
 }
 export default Confirn;
